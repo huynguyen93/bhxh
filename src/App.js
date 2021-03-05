565098baa321cdac3f6e1e2e23c42f47236a7678
Huy Nguyen <nkhuy1609@gmail.com>
2021-03-05 08:19:41 +0700

    change to table

--- src/App.js
+++ src/App.js
@@ -93,163 +93,179 @@ function App() {
     <div className="App">
       <h1>BHXH</h1>
       <hr/>
-      {periods.map((period, index) => {
-        const {
-          salary,
-          monthStart,
-          monthEnd,
-          yearStart,
-          yearEnd,
-          errorMessage,
-          amountPaidForInsurance,
-        } = period;
-        let isValidTimeRange = true;
-        if (monthStart && monthEnd && yearStart && yearEnd) {
-          if (yearEnd < yearStart || (yearEnd === yearStart && monthEnd < monthStart)) {
-            isValidTimeRange = false;
-          }
-        }
+      <table className="table table-bordered table-responsive-md">
+        <thead>
+        <tr>
+          <td>#</td>
+          <td>Từ... </td>
+          <td>Đến... </td>
+          <td>
+            Lương đóng BHXH
+            <button
+              className="btn btn-sm btn-link mx-1"
+              onClick={() => setShowModalSalary(true)}
+              disabled={showModalSalary}
+            >
+              *
+            </button>
+          </td>
+          <td>Vùng</td>
+          <td></td>
+        </tr>
+        </thead>
+        <tbody>
+          {periods.map((period, index) => {
+            const {
+              salary,
+              monthStart,
+              monthEnd,
+              yearStart,
+              yearEnd,
+              errorMessage,
+              amountPaidForInsurance,
+            } = period;
+            let isValidTimeRange = true;
+            if (monthStart && monthEnd && yearStart && yearEnd) {
+              if (yearEnd < yearStart || (yearEnd === yearStart && monthEnd < monthStart)) {
+                isValidTimeRange = false;
+              }
+            }
 
-        return (
-          <div className="row my-4" key={index}>
-            <div className="col-lg-6">
-              <div className="card">
-                <div className="card-header">
-                  <strong>#{index + 1}</strong>
-                  <span className="float-right">
-                    <button
-                      title="Xóa"
-                      onClick={() => removePeriod(index)}
-                      className="close"
-                    >
-                      ×
-                    </button>
-                  </span>
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  <select
+                    id="inputState"
+                    className="mx-3"
+                    onChange={(e) => updatePeriod(index, {monthStart: e.target.value})}
+                  >
+                    <option>Tháng...</option>
+                    {months.map((label, index) => (<option value={index + 1} key={index}>{label}</option>))}
+                  </select>
+                  <select
+                    id="inputState"
+                    className=""
+                    onChange={(e) => updatePeriod(index, {yearStart: e.target.value})}
+                  >
+                    <option>Năm...</option>
+                    {years.map((label) => (<option value={label} key={label}>{label}</option>))}
+                  </select>
+                </td>
+                <td>
+                  <select
+                    id="inputState"
+                    className="mx-3"
+                    onChange={(e) => updatePeriod(index, {monthEnd: e.target.value})}
+                  >
+                    <option>Tháng...</option>
+                    {months.map((label, index) => (<option value={index + 1} key={index}>{label}</option>))}
+                  </select>
+                  <select
+                    id="inputState"
+                    onChange={(e) => updatePeriod(index, {yearEnd: e.target.value})}
+                  >
+                    <option>Năm...</option>
+                    {years.map((label) => (<option value={label} key={label}>{label}</option>))}
+                  </select>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    placeholder="10 000 000"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value={salary}
+                    onChange={(e) => handleSalaryChange(index, e.target.value)}
+                    style={{width: '120px'}}
+                  />
+                </td>
+                <td>
+                  {Object.keys(zoneLabels).map((zoneCode) => {
+                    const id = 'radio' + zoneCode;
+                    return (
+                      <div className="form-check form-check-inline mx-3" key={zoneCode}>
+                        <input
+                          className="form-check-input"
+                          type="radio"
+                          name="zone"
+                          id={id}
+                          value={zoneCode}
+                        />
+                        <label className="form-check-label" htmlFor={id}>{zoneLabels[zoneCode]}</label>
+                      </div>
+                    )
+                  })}
+                </td>
+                <td>
+                  <button
+                    onClick={addPeriod}
+                    className="btn btn-sm btn-success mr-2"
+                  >
+                    <b>+</b>
+                  </button>
+                  <button
+                    title="Xóa"
+                    onClick={() => removePeriod(index)}
+                    className="btn btn-sm rounded btn-danger"
+                  >
+                    <b>-</b>
+                  </button>
+
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <ul>
+        {periods.map((period, index) => {
+          const {
+            monthStart,
+            monthEnd,
+            yearStart,
+            yearEnd,
+            errorMessage,
+            amountPaidForInsurance,
+          } = period;
+          let isValidTimeRange = true;
+          if (monthStart && monthEnd && yearStart && yearEnd) {
+            if (yearEnd < yearStart || (yearEnd === yearStart && monthEnd < monthStart)) {
+              isValidTimeRange = false;
+            }
+          }
+
+          if (!amountPaidForInsurance && isValidTimeRange && !errorMessage) {
+            return null;
+          }
+
+          return (
+            <li key={index}>
+              {amountPaidForInsurance && (
+                <div className="mt-3">
+                  Số tiền đóng BHXH:
+                  <ul>
+                    <li>Công ty đóng ({percentageCompanyPays}%): {utils.formatNumber(amountPaidForInsurance.byCompany)}</li>
+                    <li>Người lao động đóng ({percentageWorkerPays}%): {utils.formatNumber(amountPaidForInsurance.byWorker)}</li>
+                    <li>Tổng cộng: <strong>{utils.formatNumber(amountPaidForInsurance.total)}đ / tháng</strong></li>
+                  </ul>
                 </div>
-                <div className="card-body">
-                  <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Từ: </label>
-                    <select
-                      id="inputState"
-                      className="mx-3"
-                      onChange={(e) => updatePeriod(index, {monthStart: e.target.value})}
-                    >
-                      <option>Tháng...</option>
-                      {months.map((label, index) => (<option value={index + 1} key={index}>{label}</option>))}
-                    </select>
-                    <select
-                      id="inputState"
-                      className=""
-                      onChange={(e) => updatePeriod(index, {yearStart: e.target.value})}
-                    >
-                      <option>Năm...</option>
-                      {years.map((label) => (<option value={label} key={label}>{label}</option>))}
-                    </select>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Đến: </label>
-                    <select
-                      id="inputState"
-                      className="mx-3"
-                      onChange={(e) => updatePeriod(index, {monthEnd: e.target.value})}
-                    >
-                      <option>Tháng...</option>
-                      {months.map((label, index) => (<option value={index + 1} key={index}>{label}</option>))}
-                    </select>
-                    <select
-                      id="inputState"
-                      onChange={(e) => updatePeriod(index, {yearEnd: e.target.value})}
-                    >
-                      <option>Năm...</option>
-                      {years.map((label) => (<option value={label} key={label}>{label}</option>))}
-                    </select>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="inputSalary">Mức lương</label>
-                    <select
-                      id="inputSalary"
-                      className="mx-2"
-                      onChange={(e) => updatePeriod(index, {yearStart: e.target.value})}
-                    >
-                      {/*<option value={salaryTypes.contracted}>chính thức</option>*/}
-                      <option value={salaryTypes.insurance}>đóng BHXH</option>
-                    </select>
-                    <input
-                      type="text"
-                      placeholder="10 000 000"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      value={salary}
-                      onChange={(e) => handleSalaryChange(index, e.target.value)}
-                      style={{width: '120px'}}
-                    />
-                    <span className="text-secondary"> đ</span>
-                    <button
-                      className="btn btn-sm btn-link mx-1"
-                      onClick={() => setShowModalSalary(true)}
-                      disabled={showModalSalary}
-                    >
-                      *Giải thích
-                    </button>
-                  </div>
-                  <div>
-                    Vùng
-                    {' '}
-                    {Object.keys(zoneLabels).map((zoneCode) => {
-                      const id = 'radio' + zoneCode;
-                      return (
-                        <div className="form-check form-check-inline mx-3" key={zoneCode}>
-                          <input
-                            className="form-check-input"
-                            type="radio"
-                            name="zone"
-                            id={id}
-                            value={zoneCode}
-                          />
-                          <label className="form-check-label" htmlFor={id}>{zoneLabels[zoneCode]}</label>
-                        </div>
-                      )
-                    })}
-                    <button
-                      className="btn btn-sm btn-link mx-1"
-                      onClick={() => setShowModalZone(true)}
-                      disabled={showModalZone}
-                    >
-                      *Giải thích
-                    </button>
-                  </div>
-                  {amountPaidForInsurance && (
-                    <div className="mt-3">
-                      Số tiền đóng BHXH:
-                      <ul>
-                        <li>Công ty đóng ({percentageCompanyPays}%): {utils.formatNumber(amountPaidForInsurance.byCompany)}</li>
-                        <li>Người lao động đóng ({percentageWorkerPays}%): {utils.formatNumber(amountPaidForInsurance.byWorker)}</li>
-                        <li>Tổng cộng: <strong>{utils.formatNumber(amountPaidForInsurance.total)}đ / tháng</strong></li>
-                      </ul>
-                    </div>
-                  )}
-                  {!isValidTimeRange && (
-                    <div>
-                      <span className="text-danger">Thời gian không phù hợp, vui lòng chọn lại!</span>
-                    </div>
-                  )}
-                  {errorMessage && (
-                    <div>
-                      <span className="text-danger">{errorMessage}</span>
-                    </div>
-                  )}
+              )}
+              {!isValidTimeRange && (
+                <div>
+                  <span className="text-danger">Thời gian không phù hợp, vui lòng chọn lại!</span>
                 </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-      <button
-        onClick={addPeriod}
-        className="btn btn-sm btn-success"
-      >
-        + Thêm thời gian
-      </button>
+              )}
+              {errorMessage && (
+                <div>
+                  <span className="text-danger">{errorMessage}</span>
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+
       <div className="mt-5">
         <button
           className="btn btn-lg btn-primary"
